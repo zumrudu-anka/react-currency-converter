80ab4b40d4a9a217a4f1cda9fbc7cb3746192e52
Zümrüd-ü Anka <osmandurdag@hotmail.com>
2021-02-01 00:38:46 +0300

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-<p align="center">
-  <img align="center" src="https://github.com/zumrudu-anka/currency-converter/blob/master/presentation/CurrencyConverter.gif" />
-</p>
---
+++ precache-manifest.fdff536045dcc63bf8c9fe2d65ad96c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e646cd66f719c819bd1dc0f71577fa",
+    "url": "/react-currency-converter/index.html"
+  },
+  {
+    "revision": "60ef3692b7bafc4a8a77",
+    "url": "/react-currency-converter/static/css/main.faeb02b5.chunk.css"
+  },
+  {
+    "revision": "d99da8767e7589e56ce2",
+    "url": "/react-currency-converter/static/js/2.a39aed3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-currency-converter/static/js/2.a39aed3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ef3692b7bafc4a8a77",
+    "url": "/react-currency-converter/static/js/main.dc9ccd28.chunk.js"
+  },
+  {
+    "revision": "fb2238842f591926ca16",
+    "url": "/react-currency-converter/static/js/runtime-main.03a106ca.js"
+  }
+]);
